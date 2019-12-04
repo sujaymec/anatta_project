48c8d2b9b0764a4047045f32a70ffb059c4edb4b
sujaymec <sujay.mec@gmail.com>
2019-12-04 23:49:16 +0530

    changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,24 +1,30 @@
 import React, {Component} from 'react';
-import profile from '../Assets/images/icon-profile.png'
+import profilePic from '../Assets/images/icon-profile.png'
+import shoppingBag from '../Assets/images/icon-shopping-bag.png'
 
 export default class Navbar extends Component{
 
     render(){
         return(
             <div id="navbar">
-                <ul id="left-nav">
-                    <li>Men</li>
-                    <li>Women</li>
-                </ul>
-                <div id="logo"></div>
-                <ul>
-                    <li>
-                        <img src={profile} />
-                    </li>
-                    <li>
-                        <img src="" />
-                    </li>
-                </ul>
+                <div className="container-m50 display-flex">
+                    <ul id="left-nav">
+                        <li>Men</li>
+                        <li>Women</li>
+                    </ul>
+                    <div id="logo"></div>
+                    <ul id="right-nav">
+                        <li>
+                            <img src={shoppingBag} />
+                        </li>
+                        <li>
+                            <img src={profilePic} />
+                        </li>
+                        <li>
+                            About
+                        </li>
+                    </ul>
+                </div>
             </div>
         )
     }
