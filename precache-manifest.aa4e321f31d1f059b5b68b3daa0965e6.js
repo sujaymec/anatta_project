f4e7f881aecdfb97fcc3405c9ead3d3efb2e4255
sujaymec <sujay.mec@gmail.com>
2019-12-08 21:04:20 +0530

    Updates

---
+++ precache-manifest.aa4e321f31d1f059b5b68b3daa0965e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1da8c162f4d3fb5f81e3be753b7bfc",
+    "url": "/anatta_project/index.html"
+  },
+  {
+    "revision": "7e05c6934fcf1ab64879",
+    "url": "/anatta_project/static/css/main.7693454c.chunk.css"
+  },
+  {
+    "revision": "0f7770691423049da8d0",
+    "url": "/anatta_project/static/js/2.73300dce.chunk.js"
+  },
+  {
+    "revision": "7e05c6934fcf1ab64879",
+    "url": "/anatta_project/static/js/main.285e0128.chunk.js"
+  },
+  {
+    "revision": "54366f01a2cf5ca9347a",
+    "url": "/anatta_project/static/js/runtime-main.e0c6cbb6.js"
+  }
+]);
