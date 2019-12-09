5e0b7a4709b0b960f0fb24ebf6f74445520ba09e
sujaymec <sujay.mec@gmail.com>
2019-12-09 13:10:18 +0530

    adding menu functionality

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,17 +3,30 @@ import profilePic from '../Assets/images/icon-profile.png'
 import shoppingBag from '../Assets/images/icon-shopping-bag.png'
 
 export default class Navbar extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            menuIsOpen:false
+        }
+        this.handleMenu = this.handleMenu.bind(this);
+    }
+    handleMenu(){
+        var menuIsOpen = !(this.state.menuIsOpen);
+        this.setState({
+            menuIsOpen: menuIsOpen
+        })
+    }
 
     render(){
         return(
             <div id="navbar">
                 <div className="container-m50 display-flex">
-                    <div id="hamburger-menu" className="hide-on-desktop">
+                    <div id="hamburger-menu" className="hide-on-desktop" onClick={this.handleMenu}>
                         <div className="hamburger-menu-icon"></div>
                         <div className="hamburger-menu-icon"></div>
                         <div className="hamburger-menu-icon"></div>
                     </div>
-                    <ul id="left-nav" className="hide-on-mobile">
+                    <ul id="left-nav" className={this.state.menuIsOpen ? "" : "hide-on-mobile"}>
                         <li>Men</li>
                         <li>Women</li>
                     </ul>
